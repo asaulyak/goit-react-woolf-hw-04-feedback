248e2bb2ff1c1a78ac50c7598108a154726faae4
Eugene Asaulyak <eugene.asaulyak@gmail.com>
2024-04-26 21:02:59 +0300

    minor fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Section } from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
@@ -10,22 +10,12 @@ export const App = () => {
     neutral: 0,
     bad: 0,
   });
-  const [total, setTotal] = useState(0);
-  const [positivePercentage, setPositivePercentage] = useState(0);
 
-  useEffect(() => {
-    const totalUpdated = Object.values(stats).reduce(
-      (acc, current) => acc + current,
-      0
-    );
+  const calculateTotal = () =>
+    Object.values(stats).reduce((acc, current) => acc + current, 0);
 
-    const positivePercentageUpdated = Math.floor(
-      (stats.good / totalUpdated) * 100
-    );
-
-    setTotal(totalUpdated);
-    setPositivePercentage(positivePercentageUpdated);
-  }, [stats]);
+  const calculatePositivePercentage = () =>
+    Math.floor((stats.good / calculateTotal()) * 100);
 
   const onLeaveFeedback = key => {
     setStats(prev => ({
@@ -34,6 +24,7 @@ export const App = () => {
     }));
   };
 
+  const total = calculateTotal();
   return (
     <div
       style={{
@@ -60,7 +51,7 @@ export const App = () => {
             neutral={stats.neutral}
             bad={stats.bad}
             total={total}
-            positivePercentage={positivePercentage}
+            positivePercentage={calculatePositivePercentage()}
           />
         ) : (
           <Notification />
